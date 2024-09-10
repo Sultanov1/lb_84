08173834c28f54eba05f3e6dee9c983e72a96e69
Oybek Sultanov <oibekzzz09@gmail.com>
2024-09-10 15:49:38 +0600

    Добавили нашу конфигурацию для подключения mongodb к приложению и его использванию.

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import config from './config';
+import userRouter from './routes/users';
 
 const app = express();
 const port = 8000;
@@ -6,10 +9,18 @@ const port = 8000;
 app.use(express.static('public'));
 app.use(express.json());
 
+app.use('/users', userRouter)
+
 const run = async () => {
+  await mongoose.connect(config.database)
+
     app.listen(port, () => {
         console.log(`Listening on port ${port}`);
     });
+
+  process.on('exit', () => {
+    mongoose.disconnect();
+  })
 }
 
 run().catch(console.error);
