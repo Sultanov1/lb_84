760c301830f1e888dc000fce899143d1f0e8f38a
Oybek Sultanov <oibekzzz09@gmail.com>
2024-09-13 17:23:31 +0600

    Реализация метода delete для наших данных также исправление кода для проверки статуса нашего task.

--- routes/tasks.ts
+++ routes/tasks.ts
@@ -6,18 +6,27 @@ import User from '../models/User';
 const taskRouter = express.Router();
 const validStatuses = ['new', 'in_progress', 'complete'];
 
-taskRouter.get('/', async (req, res, next) => {
+taskRouter.get('/', async (req, res) => {
   try {
     const task = await Task.find();
     res.send(task);
   } catch (error) {
     res.status(500).send({error: 'Invalid Server Error'});
-    return next(error);
   }
 });
 
 taskRouter.post('/', async (req, res, next) => {
   try {
+    const user = await User.findById(req.body.user);
+
+    if (!user) {
+      return res.status(400).json({error: 'User not found'});
+    }
+
+    if (!validStatuses.includes(req.body.status)) {
+      return res.status(400).json({error: 'Invalid status provided'});
+    }
+
     const taskData: TaskMutation = {
       user: req.body.user,
       title: req.body.title,
@@ -27,17 +36,6 @@ taskRouter.post('/', async (req, res, next) => {
 
     const task = new Task(taskData);
 
-
-    if (!validStatuses.includes(req.body.status)) {
-      return res.status(400).json({error: 'Invalid status provided'});
-    }
-
-    const user = await User.findById(req.body.user);
-
-    if (!user) {
-      return res.status(400).json({error: 'User not found'});
-    }
-
     await task.save();
     res.send(task);
   } catch (error) {
@@ -45,36 +43,59 @@ taskRouter.post('/', async (req, res, next) => {
   }
 });
 
-  taskRouter.put('/:id', async (req, res ,next) => {
-    try {
-      const userId = req.body.user;
-      const taskId = req.params.id;
+taskRouter.put('/:id', async (req, res, next) => {
+  try {
+    const userId = req.body.user;
+    const taskId = req.params.id;
 
-      const task = await Task.findById(taskId);
+    const task = await Task.findById(taskId);
 
-      if (!task) {
-        return res.status(404).json({error: 'Task not found'});
-      }
-
-      if (task.user.toString() !== userId) {
-        return res.status(403).json({error: 'You don\'t have permission to edit this task'})
-      }
-
-      if (!validStatuses.includes(req.body.status)) {
-        return res.status(400).json({error: 'Invalid status provided'});
-      }
-
-      task.title = req.body.title || task.title;
-      task.description = req.body.description || task.description;
-      task.status = req.body.status || task.status;
-
-      await task.save();
-
-      res.json(task);
-    } catch (error) {
-      next(error);
+    if (!task) {
+      return res.status(404).json({error: 'Task not found'});
     }
-  })
+
+    if (task.user.toString() !== userId) {
+      return res.status(403).json({error: 'You don\'t have permission to edit this task'})
+    }
+
+    if (req.body.status && !validStatuses.includes(req.body.status)) {
+      return res.status(400).json({error: 'Invalid status provided'});
+    }
+
+    task.title = req.body.title || task.title;
+    task.description = req.body.description || task.description;
+    task.status = req.body.status || task.status;
+
+    await task.save();
+
+    res.json({message: 'Task was updated', task});
+  } catch (error) {
+    next(error);
+  }
+});
+
+taskRouter.delete('/:id', async (req, res, next) => {
+  try {
+    const taskId = req.params.id;
+    const userId = req.body.user;
+
+    const task = await Task.findById(taskId);
+
+    if (!task) {
+      return res.status(404).json({error: 'Task not found'});
+    }
+
+    if (task.user.toString() !== userId) {
+      return res.status(403).json({error: 'You do not have permission to delete this task'});
+    }
+
+    await Task.deleteOne({_id: taskId});
+
+    res.json({message: 'Task deleted'});
+  } catch (error) {
+    next(error);
+  }
+});
 
 
 export default taskRouter;
